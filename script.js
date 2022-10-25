17ffad15628f347f4effa4c7d267735efe2c7455
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-25 19:44:04 -0300

    Criando funcoes iniciais

---
+++ script.js
@@ -0,0 +1,11 @@
+PerguntarNome()
+PerguntarSalario()
+
+
+function PerguntarNome(){
+    var nome = prompt("Qual o nome do colaborador?")
+}
+
+function PerguntarSalario(){
+    var salario = prompt("Qual o salario do colaborador?")
+}
