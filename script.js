8bf8a16b15af20447f5ff0ec0100974fdbed9d88
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-25 20:03:31 -0300

    Criando funcoes de aumento salarial

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 PerguntarNome()
 PerguntarSalario()
-
+var salario = PerguntarSalario()
+var nome = PerguntarNome()
 
 function PerguntarNome(){
-    var nome = prompt("Qual o nome do colaborador?")
+    nome = prompt("Qual o nome do colaborador?")
+    return nome
 }
 
 function PerguntarSalario(){
-    var salario = prompt("Qual o salario do colaborador?")
+    salario = prompt("Qual o salario do colaborador?")
+    return salario
 }
+
+if(salario <= 1500){
+    salario = salario * 1.20
+}
+
+
+if(salario >= 1501 && salario <= 2000){
+    salario = salario * 1.15
+}
+
+if(salario >= 2001 && salario <3000){
+    salario = salario * 1.10
+}
+
+if(salario >= 3001){
+    salario = salario * 1.05
+}
+
+console.log(nome, salario)
