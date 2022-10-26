2f3efa38c91d6901a892804467b97349a7569d9b
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-25 21:08:29 -0300

    Ajustes simples

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var salario
 var nome
 var aumento = " "
 var continuar = 's'
+var novoSalario
 
 function PerguntarNome(){
     nome = prompt("Qual o nome do colaborador?")
@@ -11,23 +12,23 @@ function PerguntarSalario(){
     salario = prompt("Qual o salario do colaborador?")
 
     if(salario <= 1500){
-        salario = salario * 1.20
+        novoSalario = salario * 1.20
         aumento = "20%"
     }
     
     
     if(salario >= 1501 && salario <= 2000){
-        salario = salario * 1.15
+        novoSalario = salario * 1.15
         aumento = "15%"
     }
     
     if(salario >= 2001 && salario <=3000){
-        salario = salario * 1.10
+        novoSalario = salario * 1.10
         aumento = "10%"
     }
     
     if(salario >= 3001){
-        salario = salario * 1.05
+        novoSalario = salario * 1.05
         aumento = "5%"
     }
 }
@@ -40,5 +41,5 @@ while(continuar == 's'){
     PerguntarNome()
     PerguntarSalario()
     PerguntarContinuacao()
-    console.log("O nome do colaborador e:", nome, "e seu salario, com o aumento de", aumento,  "e:", salario)
- }
+    console.log("O nome do colaborador e:", nome, ", o salario sem ajustes e:", salario, ", mas com o aumento de:", aumento, ", o novo salario e:", novoSalario)
+}
